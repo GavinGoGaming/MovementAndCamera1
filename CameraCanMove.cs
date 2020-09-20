8e3f9629db04cbc85aead6cacac02ca214ea28a6
GavinGoGaming <57292172+GavinGoGaming@users.noreply.github.com>
2020-09-20 11:36:15 -0700

    All the code
    code code code.

---
+++ CameraCanMove.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraCanMove : MonoBehaviour
+{
+
+    public Transform PlayerXYZ;
+
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = PlayerXYZ.transform.position + offset;
+    }
+}
---
+++ PlayerCanMove.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerCanMove : MonoBehaviour
+{
+
+    public Rigidbody PlayerRB1;
+
+    public float ForwardForce = 200f;
+
+    public float BackwardForce = -200f;
+
+    public float LeftForce = 200f;
+
+    public float RightForce = -200f;
+
+    // Update is called once per frame 
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            PlayerRB1.AddForce(ForwardForce, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            PlayerRB1.AddForce(BackwardForce, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            PlayerRB1.AddForce(0, 0, RightForce);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            PlayerRB1.AddForce(0, 0, LeftForce);
+        }
+    }
+}
